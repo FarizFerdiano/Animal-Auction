7cdb1e00ce0aaad1a830aaf4c799fc43813aaa85
Fariz Ferdiano <106507632+FarizFerdiano@users.noreply.github.com>
2023-02-18 18:25:59 +0700

    create noneaution

---
+++ resources/views/pages/page/noneauction.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+@section('judul', 'Home')
+@section('app')
+
+{{-- content auctions --}}
+{{-- Headline --}}
+<div class="container p-0">
+    <div class="d-flex justify-content-between align-items-center px-3 mb-3">
+        <div class="Headline_home text-light d-flex justify-content text-align-left py-3">
+            <h1 class="H1_ijo">Auctions</h1>
+        </div>
+        <div class="browese">
+            <a href="/home" class="btn btn-outline-success btn-block fw-bold">
+                Back Home<img src="">
+            </a>
+        </div>
+    </div>
+</div>
+{{-- Search --}}
+<div class="container">
+    <div class="card bg-success-subtle border-success p-1 my-3 mb-4">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Cari Hewan lelang yang anda inginkan" aria-label="Search">
+            <a href="#" class="btn btn-success d-flex" type="submit"><img src="{{ asset('icons/feather_FFFFFF/search.svg') }}">
+                <span class="ms-1">
+                    Search
+                </span>
+            </a>
+        </form>
+    </div>
+</div>
+
+    </div>
+  </div>
+</section>
+@endsection 
