f140452d71e81a54192fcab8c851763a4cb91f25
Fariz Ferdiano <106507632+FarizFerdiano@users.noreply.github.com>
2023-03-05 19:14:56 +0700

    update minor views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,25 +24,29 @@
               <div class="card-body">
                   <form action="{{ route('login') }}" method="POST">
                     @csrf
-                      {{-- <!-- Username input --> --}}
-                      <div class="form-floating mb-2">
-                          <input type="text" class="form-control" id="floatingInput" name="username" placeholder="username" autofocus>
-                          <label for="floatingInput">Username</label>
-                      </div>
-                      <p class="small mt-1 pt-1 fw-regular text-muted"><img
-                              src="{{ asset('assets/icons/feather_BBBBBB/alert-circle.svg') }}"> Masukan username</p>
-  
-                      {{-- <!-- Password input --> --}}
-                      <div class="form-floating mb-2">
-                          <input type="password" class="form-control" name="password" id="floatingInput"
-                              placeholder="password">
-                          <label for="floatingInput">Password</label>
-                      </div>
-                      <p class="small mt-1 pt-1 fw-regular text-muted"><img
-                              src="{{ asset('assets/icons/feather_BBBBBB/alert-circle.svg') }}"> Masukan passowrd dengan benar</p>
+                    {{-- <!-- Username input --> --}}
+                    <div class="form-floating mb-3">
+                        <input type="text" name="username" id="username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username') }}" placeholder="Enter username"/>
+                        <label class="form-label" for="username">Username<span class="text-danger">*</span></label>
+                    @error('username')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
+                    </div>  
+                    {{-- <!-- Password input --> --}}
+                    <div class="form-floating mb-3">
+                        <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter password"/>
+                        <label class="form-label" for="password">Password<span class="text-danger">*</span></label>
+                    @error('password')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
+                    </div>    
   
                       {{-- <!-- Login & Sign button --> --}}
-                      <div class="container p-0">
+                      <div class="container p-0 mb-2">
                           <div class="d-flex w-100 gap-2 flex-column flex-md-row">
                               <button type="submit"
                                   class="col-12 col-md-6 btn btn-success btn-block text-light py-3 fw-bold">
@@ -51,9 +55,13 @@
                               </button>
                               <a href="{{ route('register') }}" class="col-12  col-md-6 btn btn-outline-success py-3 fw-bold">Registrasi</a>
                           </div>
-                          <p class="small mt-2 pt-1 fw-semibold">kembali ke
-                            {{-- <a href="auth/home" class="link-success">Home</a> --}}
+                          <p class="small mt-1 pt-1 fw-regular text-muted"><img
+                            src="{{ asset('assets/icons/feather_BBBBBB/alert-circle.svg') }}">
+                            untuk mengikutilelang anda diharuskan membuat akun terlebih dahulu !
                           </p>
+                          {{-- <p class="small mt-2 pt-1 fw-semibold">kembali ke
+                            <a href="auth/home" class="link-success">Home</a>
+                          </p> --}}
                       </div>
                   </form>
               </div>
